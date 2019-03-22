a09a0479f2f6fc27e4f569199cb30142e8139ed5
hongweimachene <hchen00@stuy.edu>
2019-03-22 11:33:00 -0400

    worked on resize

--- MyDeque.java
+++ MyDeque.java
@@ -33,17 +33,34 @@ public class MyDeque<E>{
     return s+="}";
   }
 
+  private void resize(){
+    @SuppressWarnings("unchecked")
+    E[] copy = (E[])new Object[size*2];
+    for (int i = 0; i < data.length; i++ ){
+      copy[i] = data[i];
+    }
+    data = copy;
+  }
+
   public void addFirst(E element){
     if (size > 0) {
-      if (start - 1 >= 0) {
-        start--;
-        data[start] = element;
+      if (size <= data.length) {
+        if (start - 1 >= 0) {
+          start--;
+          data[start] = element;
+        } else {
+          start = data.length-1;
+          data[start] = element;
+        }
       }
+    } else {
+      data[start] = element;
     }
-
     size++;
   }
+
   public void addLast(E element){
+    if ()
     size++;
   }
   public E removeFirst(){
