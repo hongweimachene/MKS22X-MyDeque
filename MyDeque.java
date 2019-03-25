0bfe6950cd642aac7b4f44c2ec4024b7a7a8ddcf
hongweimachene <hchen00@stuy.edu>
2019-03-25 00:36:28 -0400

    fixed resize and tested

--- MyDeque.java
+++ MyDeque.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
@@ -23,12 +24,29 @@ public class MyDeque<E>{
 
   public String toString(){
     String s = "{";
-    for (int i = 0; i < data.length; i++) {
-      if (i == data.length-1){
-        s+=data[i];
-      } else {
+    if (start < end) {
+      for (int i = start; i < end+1; i++) {
+        if (data[i] != null) {
+          if (i == end){
+            s+=data[i];
+          } else {
+            s+=data[i] + " ";
+          }
+        }
+      }
+    } else if (start > end) {
+      for (int i = start; i < data.length; i++) {
         s+=data[i] + " ";
       }
+      for (int i = 0; i < end + 1; i++) {
+        if (i == end) {
+          s+= data[i];
+        } else {
+          s+=data[i] + " ";
+        }
+      }
+    } else {
+      if (data[start] != null) s+=data[start];
     }
     return s+="}";
   }
@@ -36,13 +54,30 @@ public class MyDeque<E>{
   private void resize(){
     @SuppressWarnings("unchecked")
     E[] copy = (E[])new Object[size*2];
-    for (int i = 0; i < data.length; i++ ){
-      copy[i] = data[i];
+    int idx = 0;
+    if (start < end) {
+      for (int i = start; i < end+1; i++){
+        copy[idx] = data[i];
+        idx++;
+      }
+    } else if (start > end) {
+      for (int i = start; i < data.length; i++) {
+        copy[idx] = data[i];
+        idx++;
+      }
+      idx=0;
+      for (int i = 0; i < end + 1; i++) {
+        copy[idx+(data.length-start)] = data[i];
+        idx++;
+      }
+      start = 0;
+      end = data.length - 1;
     }
     data = copy;
   }
 
   public void addFirst(E element){
+    if (element == null) throw new NullPointerException();
     if (size > 0) {
       if (size + 1 > data.length) {
         resize();
@@ -61,6 +96,7 @@ public class MyDeque<E>{
   }
 
   public void addLast(E element){
+    if (element == null) throw new NullPointerException();
     if (size > 0){
       if (size+1 > data.length) {
         resize();
@@ -78,6 +114,7 @@ public class MyDeque<E>{
     size++;
   }
   public E removeFirst(){
+    if (size == 0) throw new NoSuchElementException();
     E removed = getFirst();
     if (size > 1) {
       if (start+1 < data.length) {
@@ -95,6 +132,7 @@ public class MyDeque<E>{
   }
 
   public E removeLast(){
+    if (size == 0) throw new NoSuchElementException();
     E removed = getLast();
     if (size > 1) {
       if (end - 1 >= 0) {
@@ -104,14 +142,27 @@ public class MyDeque<E>{
         data[end] = null;
         end = data.length - 1;
       }
+    } else {
+      data[end] = null;
     }
     size--;
     return removed;
   }
+
   public E getFirst(){
+    if (size == 0) throw new NoSuchElementException();
     return data[start];
   }
   public E getLast(){
+    if (size == 0) throw new NoSuchElementException();
     return data[end];
   }
+
+  public void printdata() {
+    for (int i = 0; i < data.length; i++) {
+      System.out.print(data[i] + " ");
+    }
+    System.out.println(" ");
+  }
+  
 }
