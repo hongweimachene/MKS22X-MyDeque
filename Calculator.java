7016c311ae9e8f6695f2bdc343670fdac49ce716
hongweimachene <hchen00@stuy.edu>
2019-03-25 11:46:14 -0400

    added isinteger function

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Calculator{
+  /*Evaluate a postfix expression stored in s.
+     *Assume valid postfix notation, separated by spaces.
+     */
+    public static double eval(String s){
+      String[] data = s.split(" ");
+      MyDeque<Double> stack = new MyDeque<Double>();
+      double result = 0;
+      for (int i = 0; i < data.length; i++) {
+        if (isInteger(data[i])) {
+          stack.addFirst(Double.valueOf(data[i]));
+        } else {
+          if (data[i].equals("+")) {
+            result = stack.removeFirst() + stack.removeFirst();
+          }
+        }
+      }
+
+
+
+    }
+
+    public static boolean isInteger(String s) {
+      try {
+        Integer.parseInt(s);
+      } catch (NumberFormatException e){
+        return false;
+      } catch (NullPointerException e) {
+        return false;
+      }
+      return true;
+    }
+}
