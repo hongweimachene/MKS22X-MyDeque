dfe5a95a1b47f7c20a266502f5b93f81241a9d24
hongweimachene <hchen00@stuy.edu>
2019-03-26 11:24:13 -0400

    fixed eval

--- Calculator.java
+++ Calculator.java
@@ -6,27 +6,25 @@ public class Calculator{
     public static double eval(String s){
       String[] data = s.split(" ");
       MyDeque<Double> stack = new MyDeque<Double>();
-      double result = 0;
       for (int i = 0; i < data.length; i++) {
         if (isInteger(data[i])) {
           stack.addFirst(Double.valueOf(data[i]));
+          System.out.println(stack);
         } else {
           if (data[i].equals("+")) {
-            result = stack.removeFirst() + stack.removeFirst();
+            stack.addLast(stack.removeFirst() + stack.removeFirst());
           } else if (data[i].equals("-")) {
-            result = stack.removeFirst() - stack.removeFirst();
+            stack.addLast(stack.removeFirst() - stack.removeFirst());
           } else if (data[i].equals("*")) {
-            result = stack.removeFirst() * stack.removeFirst();
+            stack.addLast(stack.removeFirst() * stack.removeFirst());
           } else if (data[i].equals("/")) {
-            result = stack.removeFirst() / stack.removeFirst();
+            stack.addLast(stack.removeFirst() / stack.removeFirst());
           } else if (data[i].equals("%")) {
-            result = stack.removeFirst() % stack.removeFirst();
+            stack.addLast(stack.removeFirst() % stack.removeFirst());
           }
         }
       }
-
-
-
+      return stack.getFirst();
     }
 
     public static boolean isInteger(String s) {
@@ -38,5 +36,11 @@ public class Calculator{
         return false;
       }
       return true;
+    }
+
+    public static void main(String[] args) {
+      System.out.println(eval("3 5 +"));
+      System.out.println(eval("3 5 + 8 *"));
+      System.out.println(eval("3 5 + 8 * 4 4 + /"));
     }
 }
